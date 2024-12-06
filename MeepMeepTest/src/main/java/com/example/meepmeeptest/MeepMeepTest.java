58d8c57fb50e8d78eb83d08ac32de6508b863721
KarstenVun43 <karsten.vun@gmail.com>
2024-12-06 18:53:25 -0500

    E

--- MeepMeepTest/src/main/java/com/example/meepmeeptest/MeepMeepTest.java
+++ MeepMeepTest/src/main/java/com/example/meepmeeptest/MeepMeepTest.java
@@ -14,10 +14,22 @@ public class MeepMeepTest {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(48,-58, Math.toRadians(90)))
-                .lineToLinearHeading(new Pose2d(0,-35, Math.toRadians(90)))
-                .lineTo(new Vector2d(48,-38))
-                .build());
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(48,-58, 3.14159268979/2))
+                        .lineTo(new Vector2d(48,-36))
+                        .lineTo(new Vector2d(11,-33))
+                        .lineTo(new Vector2d(58,-36))
+                        .lineTo(new Vector2d(9,-33))
+                        .lineTo(new Vector2d(69,-36))
+                        .lineTo(new Vector2d(7,-33))
+                        .waitSeconds(3)
+                        .lineTo(new Vector2d(5,-33))
+                        .waitSeconds(3)
+                        .lineTo(new Vector2d(3,-33))
+                        .waitSeconds(3)
+                        .lineTo(new Vector2d(1,-33))
+                        .waitSeconds(5)
+                        .lineTo(new Vector2d(58,-58))
+                        .build());
 
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
