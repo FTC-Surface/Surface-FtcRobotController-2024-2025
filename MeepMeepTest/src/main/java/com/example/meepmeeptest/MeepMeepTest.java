af944453759e1859db050d77cd81efd8322cb361
AJK <103875831+bello89808m@users.noreply.github.com>
2024-12-06 18:59:09 -0500

    rr

--- MeepMeepTest/src/main/java/com/example/meepmeeptest/MeepMeepTest.java
+++ MeepMeepTest/src/main/java/com/example/meepmeeptest/MeepMeepTest.java
@@ -14,21 +14,8 @@ public class MeepMeepTest {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(48,-58, 3.14159268979/2))
-                        .lineTo(new Vector2d(48,-36))
-                        .lineTo(new Vector2d(11,-33))
-                        .lineTo(new Vector2d(58,-36))
-                        .lineTo(new Vector2d(9,-33))
-                        .lineTo(new Vector2d(69,-36))
-                        .lineTo(new Vector2d(7,-33))
-                        .waitSeconds(3)
-                        .lineTo(new Vector2d(5,-33))
-                        .waitSeconds(3)
-                        .lineTo(new Vector2d(3,-33))
-                        .waitSeconds(3)
-                        .lineTo(new Vector2d(1,-33))
-                        .waitSeconds(5)
-                        .lineTo(new Vector2d(58,-58))
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(7.5,-58, 3.14159268979/2))
+                        .lineTo(new Vector2d(7.5,-36))
                         .build());
 
 
