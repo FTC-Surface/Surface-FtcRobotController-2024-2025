615761fc78d24c36530a5d7c64fe3e59d5ca837e
KarstenVun43 <karsten.vun@gmail.com>
2024-12-06 19:14:44 -0500

    strat v1
    roadrunner concept code

--- MeepMeepTest/src/main/java/com/example/meepmeeptest/MeepMeepTest.java
+++ MeepMeepTest/src/main/java/com/example/meepmeeptest/MeepMeepTest.java
@@ -14,8 +14,26 @@ public class MeepMeepTest {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(47.5,-58, 3.14159268979/2))
-                        .lineTo(new Vector2d(7.5,-36))
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(7.5,-58, 3.14159268979/2))
+                        .lineTo(new Vector2d(7.5,-34))
+
+                        .lineTo(new Vector2d(48,-37))
+                        .lineToLinearHeading(new Pose2d(48, -48, Math.toRadians(270)))
+
+                        .lineToLinearHeading(new Pose2d(58, -37, Math.toRadians(90)))
+                        .lineToLinearHeading(new Pose2d(58, -48, Math.toRadians(270)))
+
+                        .lineToLinearHeading(new Pose2d(69, -37, Math.toRadians(90)))
+                        .lineToLinearHeading(new Pose2d(69, -48, Math.toRadians(270)))
+
+                        .lineToLinearHeading(new Pose2d(45, -58, Math.toRadians(270)))
+                        .lineToLinearHeading(new Pose2d(7.5, -34, Math.toRadians(90)))
+
+                        .lineToLinearHeading(new Pose2d(45, -58, Math.toRadians(270)))
+                        .lineToLinearHeading(new Pose2d(9.5, -34, Math.toRadians(90)))
+
+                        .lineToLinearHeading(new Pose2d(45, -58, Math.toRadians(270)))
+                        .lineToLinearHeading(new Pose2d(11.5, -34, Math.toRadians(90)))
 
                         .build());
 
