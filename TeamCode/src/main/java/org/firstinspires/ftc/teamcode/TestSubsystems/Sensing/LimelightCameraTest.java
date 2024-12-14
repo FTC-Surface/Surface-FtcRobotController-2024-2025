19d34d1dbb44fb31457c60962eb260f052c62ea4
tobemuoto <tobemuoto@gmail.com>
2024-12-14 14:19:41 -0500

    Update LimelightCameraTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSubsystems/Sensing/LimelightCameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSubsystems/Sensing/LimelightCameraTest.java
@@ -1,4 +1,21 @@
 package org.firstinspires.ftc.teamcode.TestSubsystems.Sensing;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.Subsystems.Subsystem;
+
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
 public class LimelightCameraTest {
+    OpenCvWebcam webcam1;
+
+    public void init(HardwareMap hardwareMap) {
+        WebcamName webcamName1 = hardwareMap.get(WebcamName.class, "webcam1");
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam1 = OpenCvCameraFactory.getInstance().createWebcam(webcamName1, cameraMonitorViewId);
+    }
 }
