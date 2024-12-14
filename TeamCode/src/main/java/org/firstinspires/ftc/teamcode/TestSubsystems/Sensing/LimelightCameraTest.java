3bcddaa0d193247caa19b6ebf6df10793f27eb24
tobemuoto <tobemuoto@gmail.com>
2024-12-14 15:16:05 -0500

    Update LimelightCameraTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSubsystems/Sensing/LimelightCameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSubsystems/Sensing/LimelightCameraTest.java
@@ -14,8 +14,30 @@ public class LimelightCameraTest {
     OpenCvWebcam webcam1;
 
     public void init(HardwareMap hardwareMap) {
+
         WebcamName webcamName1 = hardwareMap.get(WebcamName.class, "webcam1");
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam1 = OpenCvCameraFactory.getInstance().createWebcam(webcamName1, cameraMonitorViewId);
+
+        webcam1.openCameraDeviceAsync(new OpenCvWebcam.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                webcam1.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+                webcam1.setViewportRenderingPolicy(OpenCvCamera.ViewportRenderingPolicy.OPTIMIZE_VIEW);
+            }
+            @Override
+            public void onError(int errorCode)
+            {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+    }
+
+    public void loop() {
+
     }
 }
