afa70a53161488c01a82562efce4f883213067bd
AJK <103875831+bello89808m@users.noreply.github.com>
2025-01-09 19:48:51 -0500

    Mistake fixed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSubsystems/Intake/IntakeWristTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSubsystems/Intake/IntakeWristTest.java
@@ -17,12 +17,12 @@ public class IntakeWristTest extends LinearOpMode {
     public static double lPower = 0;
     public static double rPower = 0;
 
+    public static int mode = 0;
+
     @Override
     public void runOpMode() throws InterruptedException {
         lWrist = hardwareMap.get(CRServo.class, "lWrist");
         rWrist = hardwareMap.get(CRServo.class, "rWrist");
-
-        lWrist.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
@@ -30,24 +30,29 @@ public class IntakeWristTest extends LinearOpMode {
             lWrist.setPower(lPower);
             rWrist.setPower(rPower);
 
-            if(gamepad1.a){
+            if(mode == 1){
+                lWrist.setPower(lPower);
+                rWrist.setPower(rPower);
+            }
+
+            if(gamepad1.left_stick_y > 0){
                 lWrist.setPower(0.2);
                 rWrist.setPower(0.2);
             }
 
-            if(gamepad1.b){
+            if(gamepad1.left_stick_y < 0){
                 lWrist.setPower(-0.2);
                 rWrist.setPower(-0.2);
             }
 
-            if(gamepad1.x){
-                lWrist.setPower(-0.2);
-                rWrist.setPower(0.2);
-            }
-
-            if(gamepad1.y){
+            if(gamepad1.left_stick_x > 0){
                 lWrist.setPower(0.2);
                 rWrist.setPower(-0.2);
+            }
+
+            if(gamepad1.left_stick_x < 0){
+                lWrist.setPower(-0.2);
+                rWrist.setPower(0.2);
             }
         }
     }
