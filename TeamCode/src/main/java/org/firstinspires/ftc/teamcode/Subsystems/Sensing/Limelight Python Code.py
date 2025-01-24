28a6336957b563bfd4dd1bd7dcf80ccb5264fe17
AJK <103875831+bello89808m@users.noreply.github.com>
2025-01-24 15:41:02 -0500

    Limelight python Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Sensing/Limelight Python Code.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+
+# global variables go here:
+testVar = 0
+
+def drawDecorations(image):
+    cv2.putText(image, 
+        'Limelight python script!', 
+        (0, 230), 
+        cv2.FONT_HERSHEY_SIMPLEX, 
+        .5, (0, 255, 0), 1, cv2.LINE_AA)
+    
+# runPipeline() is called every frame by Limelight's backend.
+def runPipeline(image, llrobot):
+    img_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    img_threshold = cv2.inRange(img_hsv, (20, 100, 100), (30, 255, 255))
+   
+    contours, _ = cv2.findContours(img_threshold, 
+    cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+  
+    largestContour = np.array([[]])
+    llpython = [0,0,0,0,0,0,0,0]
+
+    if len(contours) > 0:
+        cv2.drawContours(image, contours, -1, 255, 2)
+        largestContour = max(contours, key=cv2.contourArea)
+        x,y,w,h = cv2.boundingRect(largestContour)
+
+        cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,255),2)
+        llpython = [1,x,y,w,h,9,8,7]
+  
+    drawDecorations(image)
+       
+    # make sure to return a contour,
+    # an image to stream,
+    # and optionally an array of up to 8 values for the "llpython"
+    # networktables array
+    
+    return largestContour, image, llpython
