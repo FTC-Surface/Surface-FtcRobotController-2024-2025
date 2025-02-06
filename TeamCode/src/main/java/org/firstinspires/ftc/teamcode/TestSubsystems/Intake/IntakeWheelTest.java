289ed4c3a12daf40d843fa04e5f22ef1b406da67
Jack-Xie-Akioi <xjz22789@gmail.com>
2025-02-05 20:33:05 -0500

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSubsystems/Intake/IntakeWheelTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSubsystems/Intake/IntakeWheelTest.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -20,12 +21,28 @@ public class IntakeWheelTest extends LinearOpMode{
 
     public static int activateTestMode = 0;
 
+    private ColorSensor colorSensor;
 
+    private Servo armServo;
+
+    private char color;
+
+
+    public int colorRed(){return colorSensor.red();}
+    public int colorGreen(){return colorSensor.green();}
+    public int colorBlue(){return colorSensor.blue();}
+    public int alpha(){
+        return colorSensor.alpha();
+    }
     @Override
     public void runOpMode() throws InterruptedException {
 
         wheel = hardwareMap.get(DcMotorEx.class, "IntakeWheel");
         wheel.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+
+        colorSensor = hardwareMap.get(ColorSensor.class, "colorSensor");
+
+        armServo = hardwareMap.get(Servo.class,"iArm");
 
         waitForStart();
 
@@ -35,12 +52,37 @@ public class IntakeWheelTest extends LinearOpMode{
             }
 
             if(activateTestMode == 1){
-                if(gamepad1.circle){
-                    wheel.setPower(0.5);
-                } else if (gamepad1.square) {
-                    wheel.setPower(-0.5);
-                } else{
+
+                if (colorRed() > colorGreen() && colorRed() > colorBlue()) {
+                    color='r';
+                } else if (colorGreen() > colorRed() && colorGreen() > colorBlue() && colorGreen()>=200) {
+                    color='y';
+                } else if (colorBlue() > colorRed() && colorBlue() > colorGreen()) {
+                    color='b';
+                } else {
+                    color='n';
+                }
+                if(gamepad2.right_trigger >=0.5 && color!='r'){//specifically for red
+                    wheel.setPower(motorPow);
+                }
+                else if (gamepad2.dpad_left)
+                {
+                    wheel.setPower(motorPow);
+                }
+                else if (gamepad2.left_trigger >=0.5)
+                {
+                    wheel.setPower(-motorPow);
+                }
+                else{
                     wheel.setPower(0);
+                }
+                if(gamepad2.dpad_down)//Down
+                {
+                    armServo.setPosition(0);
+                }
+                if(gamepad2.dpad_up)//up
+                {
+                    armServo.setPosition(0.435);
                 }
             }
         }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSubsystems/Sensing/RevColorSensorColorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSubsystems/Sensing/RevColorSensorColorTest.java
@@ -4,6 +4,7 @@ import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @TeleOp(name = "Color Sensor Test", group = "Tests")
 @Config
