e296b1c8dca8dc219f0322e4ff09baf84ded84e9
AJK <103875831+bello89808m@users.noreply.github.com>
2025-02-09 17:21:54 -0500

    Roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/AutoOp/AutoOpHook.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/AutoOp/AutoOpHook.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.OpMode.AutoOp;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -20,6 +22,46 @@ public class AutoOpHook extends LinearOpMode {
 
         Pose2d initPos = new Pose2d(9,-60.5, Math.toRadians(90));
         MecanumDrive drive = new MecanumDrive(hardwareMap, initPos);
+
+        TrajectoryActionBuilder depositInit = drive.actionBuilder(initPos)
+                .lineToY(-26)
+                .lineToY(-40);
+
+        TrajectoryActionBuilder grabFirst = drive.actionBuilder(initPos)
+                .setReversed(true)
+                .splineToLinearHeading(new Pose2d(40, -43, Math.toRadians(65)), Math.toRadians(0))
+
+                .turn(Math.toRadians(-100))
+                .waitSeconds(0.5);
+
+        TrajectoryActionBuilder grabSecond = drive.actionBuilder(initPos)
+                .strafeToLinearHeading(new Vector2d(45,-43), Math.toRadians(55))
+                .turn(Math.toRadians(-90))
+                .waitSeconds(0.5);
+
+        TrajectoryActionBuilder grabThird = drive.actionBuilder(initPos)
+                .strafeToLinearHeading(new Vector2d(50,-43), Math.toRadians(40))
+                .turn(Math.toRadians(-110))
+                .waitSeconds(0.5);
+
+        TrajectoryActionBuilder depositFirst = drive.actionBuilder(initPos)
+                .strafeToLinearHeading(new Vector2d(40, -58), Math.toRadians(90))
+                .waitSeconds(0.5)
+                .lineToY(-60.5)
+                .setReversed(false)
+                .splineToLinearHeading(new Pose2d(6,-34, Math.toRadians(90)), Math.toRadians(90));
+
+        TrajectoryActionBuilder depositSecond = drive.actionBuilder(initPos)
+                .lineToY(-34);
+
+        TrajectoryActionBuilder depositThird = drive.actionBuilder(initPos)
+                .lineToY(-34);
+
+        TrajectoryActionBuilder depositFourth = drive.actionBuilder(initPos)
+                .lineToY(-34);
+
+        TrajectoryActionBuilder park = drive.actionBuilder(initPos)
+                .lineToY(-34);
 
         waitForStart();
 
