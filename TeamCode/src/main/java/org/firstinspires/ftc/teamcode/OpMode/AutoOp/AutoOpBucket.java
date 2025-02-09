42ea6503121776046d0e6678a359d0bece411630
AJK <103875831+bello89808m@users.noreply.github.com>
2025-02-09 17:44:15 -0500

    Roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/AutoOp/AutoOpBucket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/AutoOp/AutoOpBucket.java
@@ -1,4 +1,89 @@
 package org.firstinspires.ftc.teamcode.OpMode.AutoOp;
 
-public class AutoOpBucket {
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Newroadrunner.MecanumDrive;
+import org.firstinspires.ftc.teamcode.Robot.RobotAuto;
+
+@Config
+@Autonomous(name = "Active Intake Hook", group = "Autonomous")
+public class AutoOpBucket extends LinearOpMode {
+
+    RobotAuto robot = new RobotAuto(hardwareMap);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Pose2d initPos = new Pose2d(9,-60.5, Math.toRadians(90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initPos);
+
+        TrajectoryActionBuilder depositInit = drive.actionBuilder(initPos)
+                .strafeToLinearHeading(new Vector2d(-55,-53), Math.toRadians(45))
+                .waitSeconds(2);
+
+        TrajectoryActionBuilder grabFirst = depositInit.endTrajectory().fresh()
+                .strafeToLinearHeading(new Vector2d(-48.25,-45), Math.toRadians(90))
+                .waitSeconds(1);
+
+        TrajectoryActionBuilder depositFirst = grabFirst.endTrajectory().fresh()
+                .strafeToLinearHeading(new Vector2d(-55,-53), Math.toRadians(45))
+                .waitSeconds(2);
+
+        TrajectoryActionBuilder grabSecond = depositFirst.endTrajectory().fresh()
+                .strafeToLinearHeading(new Vector2d(-58.25,-45), Math.toRadians(90))
+                .waitSeconds(1);
+
+        TrajectoryActionBuilder depositSecond = grabSecond.endTrajectory()
+                .strafeToLinearHeading(new Vector2d(-55,-53), Math.toRadians(45))
+                .waitSeconds(2);
+
+        TrajectoryActionBuilder grabThird = depositSecond.endTrajectory().fresh()
+                .strafeToLinearHeading(new Vector2d(-60,-53), Math.toRadians(100))
+                .waitSeconds(1);
+
+        TrajectoryActionBuilder depositThird = grabThird.endTrajectory().fresh()
+                .strafeToLinearHeading(new Vector2d(-55,-53), Math.toRadians(45))
+                .waitSeconds(2);
+
+        TrajectoryActionBuilder park = depositThird.endTrajectory().fresh()
+                .splineToLinearHeading(new Pose2d(-25, 0, Math.toRadians(0)), Math.toRadians(0))
+                .waitSeconds(2);
+
+        waitForStart();
+
+        while(opModeIsActive() && !isStopRequested()){
+
+            Action depositInitAction, grabFirstAction, depositFirstAction, grabSecondAction, depositSecondAction, grabThirdAction, depositThirdAction, parkAction;
+
+            depositInitAction = depositInit.build();
+            grabFirstAction = grabFirst.build();
+            depositFirstAction = depositFirst.build();
+            grabSecondAction = grabSecond.build();
+            depositSecondAction = depositSecond.build();
+            grabThirdAction = grabThird.build();
+            depositThirdAction = depositThird.build();
+            parkAction = park.build();
+
+            Actions.runBlocking(
+                    new SequentialAction(
+                            depositInitAction,
+                            grabFirstAction,
+                            depositFirstAction,
+                            grabSecondAction,
+                            depositSecondAction,
+                            grabThirdAction,
+                            depositThirdAction,
+                            parkAction
+                    )
+            );
+        }
+    }
 }
